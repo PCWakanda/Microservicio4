66b91d3fabe1a17620722c3dae9b7796e9e441d7
ffernandoss <fsntama@myuax.com>
2024-11-28 13:05:38 +0100

    que vuelva

--- src/main/java/org/example/microservicio4/Microservicio4Application.java
+++ src/main/java/org/example/microservicio4/Microservicio4Application.java
@@ -2,12 +2,13 @@ package org.example.microservicio4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Microservicio4Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Microservicio4Application.class, args);
     }
-//
 }
