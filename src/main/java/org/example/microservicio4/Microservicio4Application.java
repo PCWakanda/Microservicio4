11c606541c8454048ea5f2219f6b881cad8dacd0
ffernandoss <fsntama@myuax.com>
2024-11-29 23:53:36 +0100

    ciwefbucwbedcb

--- src/main/java/org/example/microservicio4/Microservicio4Application.java
+++ src/main/java/org/example/microservicio4/Microservicio4Application.java
@@ -3,6 +3,7 @@ package org.example.microservicio4;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +15,9 @@ public class Microservicio4Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Microservicio4Application.class);
 
+   
     @Autowired
-    private LogService logService;
+    private RabbitMQSenderService rabbitMQSenderService;
 
     public static void main(String[] args) {
         SpringApplication.run(Microservicio4Application.class, args);
@@ -24,5 +26,11 @@ public class Microservicio4Application {
     @PostConstruct
     public void logApplicationStart() {
         logger.info("Application started successfully.");
+        sendMessage("Application started successfully.");
+    }
+
+    public void sendMessage(String message) {
+        rabbitMQSenderService.send(message);
+        logger.info("Message sent to RabbitMQ: {}", message);
     }
 }
